be0f01be8b7c431c1fd8d304171cb83c9417d4ef
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-05-02 22:48:57 +0400

    1

---
+++ cpp_bro_code_full_course/basics/helloworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+
+    // This is a comment
+
+    /*
+        This
+        is
+        a
+        multi-line
+        comment
+    */
+
+    // std::cout << "I like kebap!" << std::endl;
+    std::cout << "I like kebap!" << "\n";
+    std::cout << "It is very delicious!" << std::endl;
+    return 0;
+}
