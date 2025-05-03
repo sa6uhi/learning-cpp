6150ae00d9a3c0c196df4cd66b65d116cd2369bb
Sabuhi <sabuhi.nazarov11@gmail.com>
2025-05-03 19:28:02 +0400

    hypotenuse calculator finished

--- cpp_bro_code_full_course/projects/hypotenuse-calculator.cpp
+++ cpp_bro_code_full_course/projects/hypotenuse-calculator.cpp
@@ -4,5 +4,17 @@
 int main(int argc, char const *argv[])
 {
     double a, b, c;
+    
+    std::cout << "Enter side a:";
+    std::cin >> a;
+
+    std::cout << "Enter side b:";
+    std::cin >> b;
+
+    c = sqrt(pow(a, 2) + pow(b, 2));
+    std::cout << "Side c: " << c;
+
+
+
     return 0;
 }
